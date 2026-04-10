a57fb207b0813b41a76fc223fae6fa36e42807fd
JoÃ£o Trindade <saaspst2006@gmail.com>
2026-04-10 16:21:37 -0300

    feat(estoque): adiciona configuração de unicidade para código de produto

--- Backend/EstoqueService/Data/EstoqueDbContext.cs
+++ Backend/EstoqueService/Data/EstoqueDbContext.cs
@@ -8,4 +8,14 @@ public class EstoqueDbContext : DbContext
     public EstoqueDbContext(DbContextOptions<EstoqueDbContext> options) : base(options) { }
 
     public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Produto>(entidade =>
+        {
+           entidade.HasKey(p => p.Id);
+
+           entidade.HasIndex(p => p.Codigo).IsUnique(); 
+        });
+    }
 }
